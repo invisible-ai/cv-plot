71481c90637f4987a9a9512f9e0e3434f5d16dce
ericdanz <ericdanz@users.noreply.github.com>
2021-08-17 15:11:20 -0500

    Update test_plot.cpp

--- CvPlot/tests/test_plot.cpp
+++ CvPlot/tests/test_plot.cpp
@@ -1,7 +1,7 @@
 // CvPlot - https://github.com/Profactor/cv-plot
 // Copyright (c) 2019 by PROFACTOR GmbH - https://www.profactor.at/
 
-#include <catch2/catch.hpp>
+#include <catch.hpp>
 #include <CvPlot/cvplot.h>
 #include "cvMatIsEqual.h"
 #include <opencv2/imgproc.hpp>
